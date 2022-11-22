8584b0bbec582fc5318d31b78331faaca4e65954
Gaël James <gaeljames@gmail.com>
2022-11-22 20:44:31 +0100

    🗑️ Cleanup

--- CodeMirror6/NodeLib/src/index.ts
+++ CodeMirror6/NodeLib/src/index.ts
@@ -1,21 +1,24 @@
 import {basicSetup} from "codemirror"
 import {EditorView, keymap} from "@codemirror/view"
 import {EditorState, Compartment} from "@codemirror/state"
+import {cpp} from "@codemirror/lang-cpp"
+import {css} from "@codemirror/lang-css"
+import {html} from "@codemirror/lang-html"
+import {javascript} from "@codemirror/lang-javascript"
+import {json} from "@codemirror/lang-json"
 import {markdown} from "@codemirror/lang-markdown"
+import {python} from "@codemirror/lang-python"
+import {sql} from "@codemirror/lang-sql"
+import {xml} from "@codemirror/lang-xml"
 import {indentWithTab} from "@codemirror/commands"
 
 
+var dotNetHelpers: { [id: string]: any } = {}
 let language = new Compartment
 let tabSize = new Compartment
-
 let state: EditorState
 let view: EditorView
 
-export function setTabSize(view: EditorView, size: number) {
-    view.dispatch({
-        effects: tabSize.reconfigure(EditorState.tabSize.of(size))
-    })
-}
 
 export function initCodeMirror(id: string, initialText: string) {
     state = EditorState.create({
@@ -35,13 +38,18 @@ export function initCodeMirror(id: string, initialText: string) {
 
     view = new EditorView({
         state,
-        parent: document.getElementById(id) //document.body//
+        parent: document.getElementById(id)
     })
 }
 
-var dotNetHelpers: { [id: string]: any } = {};
+export function setTabSize(view: EditorView, size: number) {
+    view.dispatch({
+        effects: tabSize.reconfigure(EditorState.tabSize.of(size))
+    })
+}
+
 
 export function initDotNetHelpers(dotnetHelper: any, id: string)
 {
-    dotNetHelpers[id] = dotnetHelper;
+    dotNetHelpers[id] = dotnetHelper
 }
