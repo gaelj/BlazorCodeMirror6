8e3894c27924c944ba30e4e281807f34ad6b528b
Gaël James <gaeljames@gmail.com>
2023-12-19 13:05:33 +0100

    📝 Doc js state class

--- CodeMirror6/NodeLib/src/CmInstance.ts
+++ CodeMirror6/NodeLib/src/CmInstance.ts
@@ -1,14 +1,19 @@
 import { EditorView } from "@codemirror/view"
 import { EditorState, Compartment } from "@codemirror/state"
 
-export class CmInstance {
+
+/// <summary>
+/// Stores the state of a CodeMirror instance.
+/// </summary>
+export class CmInstance
+{
     public dotNetHelper: any
+    public state: EditorState
+    public view: EditorView
     public language: Compartment = new Compartment
     public tabSizeCompartment: Compartment = new Compartment
     public indentUnitCompartment: Compartment = new Compartment
     public tabSize: number
-    public state: EditorState
-    public view: EditorView
-    public placeholderCompartment: Compartment
+    public placeholderCompartment: Compartment = new Compartment
     public themeCompartment: Compartment = new Compartment
 }
