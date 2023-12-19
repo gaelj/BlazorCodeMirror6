8e5135fe0c75068a9718d2b5daae7513a983a4cc
Gaël James <gaeljames@gmail.com>
2023-12-19 14:52:58 +0100

    📝 Doc config class

--- CodeMirror6/NodeLib/src/CmConfig.ts
+++ CodeMirror6/NodeLib/src/CmConfig.ts
@@ -1,3 +1,6 @@
+/// <summary>
+/// Stores the initial configuration of a CodeMirror instance.
+/// </summary>
 export class CmConfig {
     public doc: string | null;
     public placeholder: string;
