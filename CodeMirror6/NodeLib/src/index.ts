7fc013a348fdced8978aec2dd58497054f4dcf03
Gaël James <gaeljames@gmail.com>
2023-12-20 16:52:41 +0100

    🎨 Explode basic setup

--- CodeMirror6/NodeLib/src/index.ts
+++ CodeMirror6/NodeLib/src/index.ts
@@ -1,10 +1,21 @@
-import { basicSetup } from "codemirror"
-import { EditorView, keymap, placeholder } from "@codemirror/view"
+import {
+    EditorView, keymap, highlightSpecialChars, drawSelection, highlightActiveLine, dropCursor,
+    rectangularSelection, crosshairCursor,
+    lineNumbers, highlightActiveLineGutter, placeholder
+} from "@codemirror/view"
 import { EditorState } from "@codemirror/state"
-import { indentWithTab } from "@codemirror/commands"
-import { indentUnit } from "@codemirror/language"
-import { autocompletion } from "@codemirror/autocomplete"
-import { CmInstance } from "./CmInstance"
+import { indentWithTab, history, historyKeymap, cursorSyntaxLeft, moveLineDown, moveLineUp,
+    selectSyntaxLeft, selectSyntaxRight, cursorSyntaxRight, selectParentSyntax, indentLess, indentMore,
+    copyLineUp, copyLineDown, indentSelection, deleteLine, cursorMatchingBracket, toggleComment, toggleBlockComment,
+    simplifySelection, insertBlankLine, selectLine
+} from "@codemirror/commands"
+import {
+    indentUnit, defaultHighlightStyle, syntaxHighlighting, indentOnInput, bracketMatching,
+    foldGutter, foldKeymap
+} from "@codemirror/language"
+import { autocompletion, completionKeymap, closeBrackets, closeBracketsKeymap } from "@codemirror/autocomplete"
+import { searchKeymap, highlightSelectionMatches } from "@codemirror/search"
+import { lintKeymap } from "@codemirror/lint"
 import { CmConfig } from "./CmConfig"
 import { languageChangeEffect, getDynamicHeaderStyling } from "./CmDynamicMarkdownHeaderStyling"
 import { getTheme } from "./CmTheme"
@@ -52,7 +63,63 @@ export function initCodeMirror(
         CMInstances[id].themeCompartment.of(getTheme(config.themeName)),
         CMInstances[id].readonlyCompartment.of(EditorState.readOnly.of(config.readOnly)),
         CMInstances[id].editableCompartment.of(EditorView.editable.of(config.editable)),
-        autocompletion()
+
+        // Basic Setup
+        lineNumbers(),
+        highlightActiveLineGutter(),
+        highlightSpecialChars(),
+        history(),
+        foldGutter(),
+        drawSelection(),
+        dropCursor(),
+        EditorState.allowMultipleSelections.of(true),
+        indentOnInput(),
+        syntaxHighlighting(defaultHighlightStyle, { fallback: true }),
+        bracketMatching(),
+        closeBrackets(),
+        autocompletion(),
+        rectangularSelection(),
+        crosshairCursor(),
+        highlightActiveLine(),
+        highlightSelectionMatches(),
+        keymap.of([
+            ...closeBracketsKeymap,
+
+            //...defaultKeymap,
+            {key: "Alt-ArrowLeft", mac: "Ctrl-ArrowLeft", run: cursorSyntaxLeft, shift: selectSyntaxLeft},
+            {key: "Alt-ArrowRight", mac: "Ctrl-ArrowRight", run: cursorSyntaxRight, shift: selectSyntaxRight},
+
+            {key: "Alt-ArrowUp", run: moveLineUp},
+            {key: "Shift-Alt-ArrowUp", run: copyLineUp},
+
+            {key: "Alt-ArrowDown", run: moveLineDown},
+            {key: "Shift-Alt-ArrowDown", run: copyLineDown},
+
+            {key: "Escape", run: simplifySelection},
+            {key: "Mod-Enter", run: insertBlankLine},
+
+            {key: "Alt-l", mac: "Ctrl-l", run: selectLine},
+            {key: "Mod-i", run: selectParentSyntax, preventDefault: true},
+
+            {key: "Mod-[", run: indentLess},
+            {key: "Mod-]", run: indentMore},
+            {key: "Mod-Alt-\\", run: indentSelection},
+
+            {key: "Shift-Mod-k", run: deleteLine},
+
+            {key: "Shift-Mod-\\", run: cursorMatchingBracket},
+
+            {key: "Mod-/", run: toggleComment},
+            {key: "Alt-A", run: toggleBlockComment},
+
+            ...searchKeymap,
+            ...historyKeymap,
+            ...foldKeymap,
+            ...completionKeymap,
+            ...lintKeymap,
+
+            indentWithTab,
+        ])
     ]
 
     CMInstances[id].state = EditorState.create({
