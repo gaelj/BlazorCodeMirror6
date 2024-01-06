5a7d459a174ff5e6074663ee0f38d0ccaa47b4b5
Gaël James <gaeljames@gmail.com>
2024-01-06 20:24:29 +0100

    🥅 Add sentry integration to examples

--- Examples.BlazorServer/Program.cs
+++ Examples.BlazorServer/Program.cs
@@ -8,6 +8,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.WebHost.UseSentry(o => {
+    o.Dsn = "https://d0ab79eee7b999c61d2c01fdf3958eeb@o4505402784546816.ingest.sentry.io/4506525909909504";
+    // When configuring for the first time, to see what the SDK is doing:
+    o.Debug = true;
+    // Set TracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+    // We recommend adjusting this value in production.
+    o.TracesSampleRate = 1.0;
+});
 
 var app = builder.Build();
 
@@ -24,6 +32,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseSentryTracing();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
