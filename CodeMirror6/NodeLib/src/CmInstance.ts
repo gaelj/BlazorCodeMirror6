ff1b75c3f35ceb2e997d0f08f865171e61200e73
Gaël James <gaeljames@gmail.com>
2024-03-03 22:34:15 +0100

    ✨ Add a command to force linter update (#121)

--- CodeMirror6/NodeLib/src/CmInstance.ts
+++ CodeMirror6/NodeLib/src/CmInstance.ts
@@ -14,6 +14,7 @@ export class CmInstance
     public view: EditorView
     public setup: CmSetup
     public config: CmConfiguration
+    public lintRefreshRequested: boolean = false
     public languageCompartment: Compartment = new Compartment
     public markdownStylingCompartment: Compartment = new Compartment
     public tabSizeCompartment: Compartment = new Compartment
