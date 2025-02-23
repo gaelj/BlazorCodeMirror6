881727cdb6090ec940445d82106a0a870c4b090b
pm64 <pm64@users.noreply.github.com>
2025-02-23 12:50:30 +0000

    Add repro project for issue #210 to CSS-path-issue/MauiApp3

---
+++ CSS-path-issue/MauiApp3/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MauiApp3
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "MauiApp3" };
+        }
+    }
+}
---
+++ CSS-path-issue/MauiApp3/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiApp3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
